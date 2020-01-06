82d7322386800721608f2fc370c8c2538b079db6
Frederico Braun <frederico@deltahub.com.br>
2020-01-06 16:33:28 -0300

    Conclusão do módulo 2 do curso

--- functions/get-index.js
+++ functions/get-index.js
@@ -6,6 +6,10 @@ const superagent = require('superagent');
 const aws4 = require('aws4');
 const URL = require('url');
 
+const awsRegion = process.env.AWS_REGION;
+const cognitoUserPoolId = process.env.cognito_user_pool_id;
+const cognitoClientId = process.env.cognito_client_id;
+
 const restaurantsApiRoot = process.env.restaurants_api;
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
@@ -39,7 +43,16 @@ module.exports.handler = async (event, context) => {
   let template = loadHtml();  
   let restaurants = await getRestaurants();
   let dayOfWeek = days[new Date().getDay()];
-  let html = Mustache.render(template, { dayOfWeek, restaurants });
+  let view = {
+    dayOfWeek, 
+    restaurants,
+    awsRegion,
+    cognitoUserPoolId,
+    cognitoClientId,
+    searchUrl: `${restaurantsApiRoot}/search`
+  }
+    
+  let html = Mustache.render(template, view);
   
   const response = {
     statusCode: 200,
