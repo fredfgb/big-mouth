bf41a261d6016c585d5d65daf6c1b1e7533f9b9c
Frederico Braun <frederico@deltahub.com.br>
2020-01-06 15:39:42 -0300

    Configuração IAM getRestaurants api

--- functions/get-index.js
+++ functions/get-index.js
@@ -3,6 +3,8 @@
 const fs = require("fs")
 const Mustache = require('mustache');
 const superagent = require('superagent');
+const aws4 = require('aws4');
+const URL = require('url');
 
 const restaurantsApiRoot = process.env.restaurants_api;
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -16,10 +18,26 @@ function loadHtml(){
   return html;
 }
 
+async function getRestaurants() {
+  let url = URL.parse(restaurantsApiRoot);
+  let opts = {
+    host: url.hostname,
+    path: url.pathname
+  };
+  aws4.sign(opts);
+
+  let res = await superagent
+                    .get(restaurantsApiRoot)
+                    .set('Host', opts.headers['Host'])
+                    .set('X-Amz-Date', opts.headers['X-Amz-Date'])
+                    .set('Authorization', opts.headers['Authorization'])
+                    .set('X-Amz-Security-Token', opts.headers['X-Amz-Security-Token']);
+  return res.body;
+}
+
 module.exports.handler = async (event, context) => {
-  let template = loadHtml();
-  let res = await superagent.get(restaurantsApiRoot);
-  let restaurants = res.body;
+  let template = loadHtml();  
+  let restaurants = await getRestaurants();
   let dayOfWeek = days[new Date().getDay()];
   let html = Mustache.render(template, { dayOfWeek, restaurants });
   
